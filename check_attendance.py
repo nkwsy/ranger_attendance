4e4e7f87c991aa35d8a2138debae12773a155d4d
chro <nick@urbanriv.org>
2019-07-29 08:49:04 -0500

    test

--- check_attendance.py
+++ check_attendance.py
@@ -34,7 +34,7 @@ try:
     # lcd.message('Place Card to\nrecord attendance')
     id, text = reader.read()
     print(id)
-    cursor.execute("Select id, firstName, lastName, phone FROM users WHERE rfid_uid=%s", (str(id)))
+    cursor.execute("Select id, firstName, lastName, phone FROM users WHERE rfid_uid=%s", (str(id),))
     result = cursor.fetchone()
     print(result)
 #Check if account is valid, sign in or out the user.
