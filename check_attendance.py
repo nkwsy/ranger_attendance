76ee3eb2cbd2c745fdc3f96423ae675ed4b8486f
nkwsy <nick@nickwesley.me>
2021-04-23 14:11:07 -0500

    euao

--- check_attendance.py
+++ check_attendance.py
@@ -179,7 +179,7 @@ def send_message_to_slack(text):
     import requests
 
     # Set the webhook_url to the one provided by Slack when you create the webhook at https://my.slack.com/services/new/incoming-webhook/
-    webhook_url = 'https://hooks.slack.com/services/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'
+    webhook_url = 'https://hooks.slack.com/services/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'
     slack_data = {"text": 'tehu'}
 
     response = requests.post(
